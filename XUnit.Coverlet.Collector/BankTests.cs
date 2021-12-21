b479206b5bea5fd006138188c260b6f845bb14a7
msrinivascharan <msrinivascharan@gmail.com>
2021-12-21 11:44:49 +0530

    fc

---
+++ XUnit.Coverlet.Collector/BankTests.cs
@@ -0,0 +1,24 @@
+
+using System; 
+using Xunit; 
+
+
+namespace XUnit.Coverlet
+{
+    public class BankAccountTests
+    {
+        //readonly BankAccount _bankAccount;
+
+        //public BankAccountTests() => _bankAccount = new BankAccount("sam",99);
+
+        [Fact ]
+        //public void IsGetCustomerNameOk() =>
+          //  Assert.NotNull( _bankAccount.Balance);
+
+      public void Test1() { 
+         Assert.True(true); 
+      } 
+
+    }
+
+}
